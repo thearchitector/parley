41dc176578aeb90be545abc8f4397c3157f7bcb3
Elias Gabriel <me@eliasfgabriel.com>
2023-11-19 16:13:15 -0500

    fix: artifacts

--- bin/optimize.sh
+++ bin/optimize.sh
@@ -7,4 +7,4 @@ SRC="public"
 svgo -rf ${SRC}/vendor/icons --precision 0
 terser ${SRC}/vendor/randomNames.js -o ${SRC}/vendor/randomNames.js
 terser --config-file terser.config.json ${SRC}/application.js -o ${SRC}/application.js
-postcss ${SRC}/style.css -o ${SRC}/style.css
+postcss ${SRC}/style.css --verbose -o ${SRC}/style.css
