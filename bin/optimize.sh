e7dc590cd6fe465344511b0ff76e34581f323801
Elias Gabriel <me@eliasfgabriel.com>
2023-11-19 16:06:50 -0500

    ci: fix path

--- bin/optimize.sh
+++ bin/optimize.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-svgo -rf public/vendor/icons --precision 0
-terser public/vendor/randomNames.js -o vendor/randomNames.js
-terser --config-file terser.config.json public/application.js -o public/application.js
-postcss public/style.css -o public/style.css
+SRC_PREFIX="public"
+
+svgo -rf ${SRC}/vendor/icons --precision 0
+terser ${SRC}/vendor/randomNames.js -o ${SRC}/vendor/randomNames.js
+terser --config-file terser.config.json ${SRC}/application.js -o ${SRC}/application.js
+postcss ${SRC}/style.css -o ${SRC}/style.css
